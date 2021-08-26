043692ad574b38aa17393448fa19416bc8308236
Timothy Coleman <timhc22@gmail.com>
2021-08-27 00:46:13 +0100

    Add data from opensea

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import './global.scss';
 import HomePage from "./pages/HomePage";
 
 function App(): JSX.Element {
 
-  useEffect(() => {
-    getAssets();
-  }, []);
-
-  async function getAssets() {
-    let assets;
-
-    assets = await fetch('https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=20&owner=0x0000000000000000000000000000000000000000');
-    assets = await assets.json();
-
-    console.log(assets);
-    return assets;
-  }
-
   return (
     <div className="App">
-      <HomePage/>
+      <HomePage />
     </div>
   );
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, Suspense } from 'react';
+import React, {useRef, useState, Suspense, useEffect} from 'react';
 import classes from './homePage.module.scss';
 import Moon from "../components/Moon";
 import {Html, OrbitControls, PerspectiveCamera, Sphere, Stars, Text} from "@react-three/drei";
@@ -8,9 +8,56 @@ import Line from "../components/Line";
 import Image3D from "../components/Image3D";
 import {Typography} from "@material-ui/core";
 
+
 export default function HomePage(): JSX.Element {
   const [open, setOpen] = React.useState(false);
+  const [gallery, setGallery] = React.useState([]);
   const [selectedImage, setSelectedImage] = React.useState({name: "", description: "", url: ""});
+
+
+  useEffect(() => {
+    getAssets();
+  }, []);
+
+  async function getAssets() {
+    let assets;
+
+    assets = await fetch('https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=20&owner=0x0000000000000000000000000000000000000000');
+    assets = await assets.json();
+
+    console.log('pulled assets:');
+    console.log(assets);
+    assets = assets.assets;
+
+    if (assets.length > 0) {
+      let newAssets = assets.map((asset: any, index: number) => {
+        let assetImage, assetImageName, assetImageDescription;
+        // if (asset.collection && asset.collection.banner_image_url) {
+        assetImage = asset.image_thumbnail_url;
+        // assetImage = asset.collection.banner_image_url;
+        if (assetImage == null) return null;
+        // assetImageName = asset.collection.name;
+        assetImageName = asset.name;
+        // assetImageDescription = asset.collection.description;
+        assetImageDescription = asset.description;
+        // }
+
+        return {
+          url: assetImage,
+          name: assetImageName,
+          description: assetImageDescription,
+          pos1: 0,
+          pos2: index - 0.5,
+          pos3: index - 2,
+          asset: asset
+        }
+      }).filter((e:any) => e);
+
+      console.log('new array:');
+      console.log(newAssets);
+      setGallery(newAssets);
+    }
+  }
 
   const handleOpen = (image: any) => {
     setOpen(true);
@@ -21,18 +68,20 @@ export default function HomePage(): JSX.Element {
     setOpen(false);
   };
 
-  const gallery = [
-    { url: "https://upload.wikimedia.org/wikipedia/en/3/3c/PinkFloyd-album-piperatthegatesofdawn_300.jpg", pos1: 0, pos2: 1, pos3: 4, name: "Piper at the Gates of Dawn", description: "The Piper at the Gates of Dawn is the debut album by English rock band Pink Floyd, the only album made under founding member Syd Barrett's leadership."},
-    { url: "https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png", pos1: 0, pos2: 0.5, pos3: 1, name: "Dark Side of the Moon", description: "The Dark Side of the Moon is the eighth studio album by the English rock band Pink Floyd, released on 1 March 1973 by Harvest Records."},
-    { url: "https://upload.wikimedia.org/wikipedia/en/7/74/Pink_Floyd-Animals-Frontal.jpg", pos1: 0, pos2: -2, pos3:-2, name: "Animals", description: "Animals is the tenth studio album by the English rock band Pink Floyd, released on 21 January 1977 through Harvest and Columbia Records."},
-    { url: "https://upload.wikimedia.org/wikipedia/en/1/13/PinkFloydWallCoverOriginalNoText.jpg", pos1: -1.5, pos2: -4, pos3:-7, name: "The Wall", description: "The Wall is the eleventh studio album by the English rock band Pink Floyd, released on 30 November 1979 by Harvest and Columbia Records." }
-  ]
+
+
+  // const gallery = [
+  //   { url: "https://upload.wikimedia.org/wikipedia/en/3/3c/PinkFloyd-album-piperatthegatesofdawn_300.jpg", pos1: 0, pos2: 1, pos3: 4, name: "Piper at the Gates of Dawn", description: "The Piper at the Gates of Dawn is the debut album by English rock band Pink Floyd, the only album made under founding member Syd Barrett's leadership."},
+  //   { url: "https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png", pos1: 0, pos2: 0.5, pos3: 1, name: "Dark Side of the Moon", description: "The Dark Side of the Moon is the eighth studio album by the English rock band Pink Floyd, released on 1 March 1973 by Harvest Records."},
+  //   { url: "https://upload.wikimedia.org/wikipedia/en/7/74/Pink_Floyd-Animals-Frontal.jpg", pos1: 0, pos2: -2, pos3:-2, name: "Animals", description: "Animals is the tenth studio album by the English rock band Pink Floyd, released on 21 January 1977 through Harvest and Columbia Records."},
+  //   { url: "https://upload.wikimedia.org/wikipedia/en/1/13/PinkFloydWallCoverOriginalNoText.jpg", pos1: -1.5, pos2: -4, pos3:-7, name: "The Wall", description: "The Wall is the eleventh studio album by the English rock band Pink Floyd, released on 30 November 1979 by Harvest and Columbia Records." }
+  // ]
 
   return (
     <div>
-      <Typography variant="subtitle1" gutterBottom>
-        Timeline Example
-      </Typography>
+      {/*<Typography variant="subtitle1" gutterBottom>*/}
+      {/*  Timeline Example*/}
+      {/*</Typography>*/}
       <Canvas className="timeline-canvas">
         {/*<PerspectiveCamera position={[4, 4, 7]} makeDefault />*/}
         <PerspectiveCamera position={[4, 7, 7]} makeDefault />
