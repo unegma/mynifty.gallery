3552c973d81e4ccb53508912eaa2ce0d60513abc
Timothy Coleman <timhc22@gmail.com>
2021-09-04 20:41:27 +0100

    Move shapes to external library

--- src/helpers/fetchAssets.ts
+++ src/helpers/fetchAssets.ts
@@ -1,4 +1,4 @@
-import {randFloat} from "three/src/math/MathUtils";
+import * as shapeUtils from '@unegma/shapes';
 
 /**
  * Offset is for using with opensea api and is used for getting past the 20 max
@@ -154,79 +154,11 @@ export default async function getAssets(assetsArray: [], maxImages: number, addr
 function createShapes(index:number) {
   let shapes = <any>[];
 
-  shapes.push(createCluster(index));
-  shapes.push(createSpiral(index));
-  shapes.push(createGoldenSpiral(index));
-  shapes.push(createSwirl(index));
+  shapes.push(shapeUtils.createCluster(index));
+  shapes.push(shapeUtils.createSpiral(index));
+  shapes.push(shapeUtils.createGoldenSpiral(index));
+  shapes.push(shapeUtils.createSwirl(index));
 
   return shapes;
 }
 
-/**
- * Shapes
- * @param index
- */
-
-function createCluster(index:number) {
-  // create co-ordinates based on the a number between 0 and the index of the item (higher index = older item)
-  // the second part randomly creates 1 or negative 1 to add depth along each axis
-  let pos1 = randFloat(0, index) * getRandomOneOrNegativeOne();
-  let pos2 = randFloat(0, index) * getRandomOneOrNegativeOne();
-  let pos3 = randFloat(0, index) * getRandomOneOrNegativeOne();
-
-  return { pos1: pos1, pos2: pos2, pos3: pos3 };
-}
-
-function createSpiral(index:number) {
-  // create co-ordinates based on the a number between 0 and the index of the item (higher index = older item)
-  // the second part randomly creates 1 or negative 1 to add depth along each axis
-  let pos1 = 7*Math.sin(index) *-1;
-  let pos2 = 7*Math.cos(index) *-1;
-  let pos3 = 7*index *-1;
-
-  return { pos1: pos1, pos2: pos2, pos3: pos3 };
-}
-
-
-function createGoldenSpiral(index:number) {
-  // // https://www.reddit.com/r/theydidthemath/comments/286tqb/3d_golden_spiral_equation/
-  const exponent = 0.306349; // growth factor in Radians: `ln(phi)/(pi/2)` OR in Degrees: `ln(phi)/90`
-  const angle = index;// theta
-// const angle = (index + 0.1) * (Math.round(Math.random()) * 2 - 1) ;// theta
-  const slope = 0.2;
-  const t = index - index / 2; // todo what is t?
-
-  let pos1 = ((Math.exp(exponent) * angle) * Math.sin(angle));
-  let pos2 = ((Math.exp(exponent) * angle) * Math.cos(angle));
-  let pos3 = ((index * (slope * angle))) / (index * Math.sqrt(index));
-
-  return { pos1: pos1, pos2: pos2, pos3: pos3 };
-}
-
-
-function createSwirl(index: number) {
-  // // https://www.reddit.com/r/theydidthemath/comments/286tqb/3d_golden_spiral_equation/
-  // const exponent2 = 0.306349; // growth factor in Radians: `ln(phi)/(pi/2)` OR in Degrees: `ln(phi)/90`
-  const exponent2 = 0.1; // growth factor in Radians: `ln(phi)/(pi/2)` OR in Degrees: `ln(phi)/90`
-  // const angle = index ;// theta
-  const angle2 = 0.5 * index;// theta
-  // const angle2 = (index + 0.1) * (Math.round(Math.random()) * 2 - 1) ;// theta
-  const slope2 = 3;
-  // const t2 = index - index/2; // todo what is t?
-
-  let pos1 = ((Math.exp(exponent2) * angle2) * Math.sin(angle2));
-  let pos2 = ((Math.exp(exponent2) * angle2) * Math.cos(angle2));
-  let pos3 = index + (slope2 * exponent2);
-  // let pos3dm4 = ((index*(slope2 * angle2))) / (index * Math.sqrt(index));
-  // let pos3dm4 = ((Math.tan(slope) * ((Math.sqrt(1+exponent) )*Math.exp(exponent * t))/exponent ));
-
-  return { pos1: pos1, pos2: pos2, pos3: pos3 };
-}
-
-
-/**
- * Low Level functions
- */
-function getRandomOneOrNegativeOne () {
-  return (Math.round(Math.random()) * 2 - 1);
-}
