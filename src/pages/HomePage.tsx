c06ee0b5430b2be850c50a863a26822f487d3b3f
Timothy Coleman <timhc22@gmail.com>
2021-09-14 13:37:34 +0100

    Set to poap by default

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -38,7 +38,7 @@ export default function HomePage(): JSX.Element {
   // const [musicUrl, setMusicUrl] = React.useState("https://www.free-stock-music.com/music/alexander-nakarada-space-ambience.mp3");
   const [address, setAddress] = React.useState<string>("");
   const [scene, setScene] = React.useState(1);
-  const [source, setSource] = React.useState<Source|string>(Source.Wallet);
+  const [source, setSource] = React.useState<Source|string>(Source.POAP);
   const [open, setOpen] = React.useState(false);
   const [dialogOpen, setDialogOpen] = React.useState(false);
   const [dialogData, setDialogData] = React.useState('');
